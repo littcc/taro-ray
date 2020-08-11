b52dbb18b1d34ba6fd371c03f291b7e35c6c5647
liupeiqiang <littcc@icloud.com>
2020-08-12 03:15:11 +0800

    refactor: 打包增加了依赖库

--- bili.config.ts
+++ bili.config.ts
@@ -1,18 +1,18 @@
 import { Config } from 'bili';
 
 const config: Config = {
-    input: './src/index.ts',
-    output: {
-        target: 'browser',
-        format: ['cjs', 'cjs-min', 'es'],
-        dir: 'lib',
-        sourceMap: true
-    },
-    bundleNodeModules: true,
-    externals: [/@tarojs/, 'lodash', 'path-to-regexp'],
-    plugins: {
-        babel: false
-    }
+  input: './src/index.ts',
+  output: {
+    target: 'browser',
+    format: ['cjs', 'cjs-min', 'es-min'],
+    dir: 'lib',
+    sourceMap: false,
+  },
+  bundleNodeModules: true,
+  externals: [/@tarojs/, 'lodash'],
+  plugins: {
+    babel: false,
+  },
 };
 
 export default config;
